968f80bc9c083420d57df927396cb10139ca4ce9
Daksh <Dakshjkhadalpur177@gmail.com>
2025-04-19 11:17:01 +0530

    fixed url issue google auth and updated .env

--- backend/src/lib/passport.js
+++ backend/src/lib/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://chatty2-0.onrender.com/api/auth/google/callback",
+      callbackURL: process.env.CALL_BACK_URI,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
